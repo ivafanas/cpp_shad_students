e2f4053f0b3c7296adb55ce4f189b2033f6e789f
Ivan Afanasyev <ivafanas@gmail.com>
2019-08-07 01:32:03 +0700

    [ ADD ] add 2019 seminar3 and lecture3 content

---
+++ 2019_sem1/seminar3_classes_1/1_ctors_order.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+
+class Name
+{
+public:
+	Name() { std::cout << "Name "; }
+	~Name() { std::cout << "~Name "; }
+};
+
+class Leg
+{
+public:
+	Leg() { std::cout << "Leg "; }
+	~Leg() { std::cout << "~Leg "; }
+};
+
+class Tail
+{
+public:
+	Tail() { std::cout << "Tail "; }
+	~Tail() { std::cout << "~Tail "; }
+};
+
+class Animal
+{
+public:
+	Animal() { std::cout << "Animal "; }
+	~Animal() { std::cout << "~Animal "; }
+
+private:
+	Name name_;
+};
+
+class Cat : public Animal
+{
+public:
+	Cat() { std::cout << "Cat "; }
+	~Cat() { std::cout << "~Cat "; }
+
+private:
+	Leg l1_;
+	Leg l2_;
+	Leg l3_;
+	Leg l4_;
+	Tail tail_;
+};
+
+
+// show this later
+// Animal animal;
+
+int main()
+{
+	std::cout << "hello!" << std::endl;
+	Cat cat;
+	std::cout << std::endl << "goodbye!" << std::endl;
+	return 0;
+}
