394fe311f15a8550fa59f5b018d42be291a9c81e
Ivan Afanasyev <ivafanas@gmail.com>
2021-09-19 19:58:58 +0700

    [ ADD ] add lab1 cmake project stub

---
+++ 2021/sem1/lab1_stub/hasher/src/hash.cpp
@@ -0,0 +1,3 @@
+
+// TODO: your implementation here
+
---
+++ 2021/sem1/lab1_stub/hasher/src/tests/hash_unittest.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+
+// TODO: your tests here
+
+TEST(function1, scenario1)
+{
+    EXPECT_EQ(1, 1);
+}
+
+TEST(function1, scenario2)
+{
+    EXPECT_EQ(2, 2);
+}
+
+TEST(function1, scenario3)
+{
+    EXPECT_EQ(1, 2);
+}
+
