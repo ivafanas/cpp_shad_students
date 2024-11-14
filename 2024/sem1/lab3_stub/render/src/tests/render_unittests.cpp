f1cb42165ab54872f1e3ea92ec4498d8676dcb6d
Ivan Afanasyev <iafanasyev@unipro.ru>
2024-11-14 18:59:32 +0700

    [ ADD ] Add stub for lab3 render 2024.s1.

---
+++ 2024/sem1/lab3_stub/render/src/tests/render_unittests.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+
+// TODO: your tests here
+
+TEST(function1, scenario1) { EXPECT_EQ(1, 1); }
+
+TEST(function1, scenario2) { EXPECT_EQ(2, 2); }
+
+TEST(function1, scenario3) { EXPECT_EQ(3, 3); }
